a2c1d27573779cac3af3be3d132ef30c18a87ede
Stary54264 <stary54264@gmail.com>
2023-10-30 17:25:08 -0400

    Finished

--- src/interface_adapter/uploadQuestions/UploadQuestionsController.java
+++ src/interface_adapter/uploadQuestions/UploadQuestionsController.java
@@ -1,15 +1,15 @@
-package interface_adapter.UploadQuestions;
+package interface_adapter.upload_questions;
 
 import use_cases.uploadQuestions.UploadQuestionsInputBoundary;
 
 public class UploadQuestionsController {
-    final UploadQuestionsInputBoundary uploadQuestionsUseCaseInteractor;
 
-    public UploadQuestionsController(UploadQuestionsInputBoundary uploadQuestionsUseCaseInteractor){
-        this.uploadQuestionsUseCaseInteractor = uploadQuestionsUseCaseInteractor;
+    final UploadQuestionsInputBoundary UploadQuestionsUseCaseInteractor;
+    public UploadQuestionsController(UploadQuestionsInputBoundary UploadQuestionsUseCaseInteractor) {
+        this.UploadQuestionsUseCaseInteractor = UploadQuestionsUseCaseInteractor;
     }
 
-    public void execute(){
-        uploadQuestionsUseCaseInteractor.execute();
+    public void execute() {
+        UploadQuestionsUseCaseInteractor.execute();
     }
 }
--- src/use_cases/uploadQuestions/UploadQuestionsUserDataAccessInterface.java
+++ src/use_cases/uploadQuestions/UploadQuestionsUserDataAccessInterface.java
@@ -1,5 +1,5 @@
 package use_cases.uploadQuestions;
 
 public interface UploadQuestionsUserDataAccessInterface {
-    void clear();
+    void addQuestions();
 }
