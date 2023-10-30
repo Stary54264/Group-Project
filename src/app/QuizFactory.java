ed896405537e6faf1747e61115aaaa614e09057d
fmwofmwmf <fmwofmwmf@gmail.com>
2023-10-30 16:24:04 -0400

    factory

---
+++ src/app/QuizFactory.java
@@ -0,0 +1,12 @@
+package app;
+
+import entity.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizFactory {
+    public static List<Question> Parse(String inp) {
+        return new ArrayList<>();
+    }
+}
--- src/use_cases/getApiQuestions/GetApiQuestionsDataAccessInterface.java
+++ src/use_cases/getApiQuestions/GetApiQuestionsDataAccessInterface.java
@@ -1,5 +1,5 @@
 package use_cases.getApiQuestions;
 
 public interface GetApiQuestionsDataAccessInterface {
-    String RetrieveQuestions(int numberOfQuestions, int questionCategory, GetApiQuestionsInputData.QuestionType questionType, GetApiQuestionsInputData.QuestionDifficulty difficulty);
+    //api access is static
 }
