4e99ef70d4864d80a89622a7f18c56b39f24e657
Ryan Chen <ryanchen604@gmail.com>
2023-10-30 14:50:36 -0400

    added files for four use cases

---
+++ src/use_cases/manageQuiz/manageQuizOutputBoundary.java
@@ -0,0 +1,4 @@
+package use_cases.manageQuiz;
+
+public interface manageQuizOutputBoundary {
+}
--- src/use_cases/takeQuiz/takeQuizInputData.java
+++ src/use_cases/takeQuiz/takeQuizInputData.java
@@ -1,4 +1,4 @@
 package src.use_cases.take_quiz;
 
-public class name4 {
+public class takeQuizInputData{
 }
