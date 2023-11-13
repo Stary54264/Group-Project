1b859d2413b2215505b5d48a1ca1dbb3c675624d
lveluesl <jessica.shenlue@gmail.com>
2023-11-13 09:40:18 -0500

    getAPI Interactor partial

--- src/use_cases/getApiQuestions/GetApiQuestionsInteractor.java
+++ src/use_cases/getApiQuestions/GetApiQuestionsInteractor.java
@@ -1,4 +1,12 @@
 package src.use_cases.getApiQuestions;
+import src.entity.Question;
+import src.use_cases.getApiQuestions.GetApiQuestionsInputData.QuestionType;
+import src.use_cases.getApiQuestions.GetApiQuestionsInputData.QuestionDifficulty;
+
+import java.io.IOException;
+import java.util.List;
+
+import static src.data_access.APIDataAccessObject.RetrieveQuestions;
 
 public class GetApiQuestionsInteractor implements GetApiQuestionsInputBoundary {
     final GetApiQuestionsDataAccessInterface apiDataAccessObject;
@@ -12,6 +20,20 @@ public class GetApiQuestionsInteractor implements GetApiQuestionsInputBoundary {
 
     @Override
     public void execute(GetApiQuestionsInputData getApiQuestionsInputData) {
-        // finish later as instructed in ppt (after the test)
+        int number = getApiQuestionsInputData.getNumberOfQuestions();
+        int category = getApiQuestionsInputData.getQuestionCategory();
+        QuestionType questionType = getApiQuestionsInputData.getQuestionType();
+        // convert questionType into int
+        int type = 0;
+        QuestionDifficulty questionDifficulty = getApiQuestionsInputData.getDifficulty();
+        // convert questionDifficulty into int
+        int difficulty = 0;
+
+        try {
+            List<Question> questions = RetrieveQuestions(number, category, difficulty, type);
+        } catch (Exception e) {
+            apiPresenter.prepareFailView("Error occurred!"); // if throws exception, fail
+        }
+        // create test with ID & save & success view
     }
 }
--- src/use_cases/getApiQuestions/GetApiQuestionsOutputBoundary.java
+++ src/use_cases/getApiQuestions/GetApiQuestionsOutputBoundary.java
@@ -3,5 +3,5 @@ package src.use_cases.getApiQuestions;
 public interface GetApiQuestionsOutputBoundary {
     void prepareSuccessView(GetApiQuestionsOutputData response);
 
-    void prepareFailView(GetApiQuestionsOutputData error);
+    void prepareFailView(String error);
 }
