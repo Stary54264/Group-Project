25d62629a242741f6aac38aaab8e10ecef1bf0ed
heyuchengzhang <heyucheng.zhang@mail.utoronto.ca>
2023-11-13 18:33:38 -0500

    update FileUserDataAccessObject

--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -4,7 +4,8 @@ import entity.Test;
 
 import use_cases.getApiQuestions.GetApiQuestionsDataAccessInterface;
 import use_cases.takeQuiz.takeQuizDataAccessInterface;
-public class FileUserDataAccessObject implements takeQuizDataAccessInterface, GetApiQuestionsDataAccessInterface {
+import use_cases.getResult.GetResultDataAccessInterface;
+public class FileUserDataAccessObject implements takeQuizDataAccessInterface, GetApiQuestionsDataAccessInterface, GetResultDataAccessInterface{
 
     @Override
     public Test getTest(String name) {
