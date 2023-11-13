6a1e6f58de47f503d7db37638f40f2e250de85b7
lveluesl <jessica.shenlue@gmail.com>
2023-11-13 09:03:50 -0500

    getAPI DAI partial

--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -2,11 +2,22 @@ package src.data_access;
 
 import src.entity.Test;
 
+import src.use_cases.getApiQuestions.GetApiQuestionsDataAccessInterface;
 import src.use_cases.takeQuiz.takeQuizDataAccessInterface;
-public class FileUserDataAccessObject implements takeQuizDataAccessInterface{
+public class FileUserDataAccessObject implements takeQuizDataAccessInterface, GetApiQuestionsDataAccessInterface {
 
     @Override
     public Test getTest(String name) {
         return null;
+    }
+
+    @Override
+    public void save(Test test) {
+        // implement later
+    }
+
+    @Override
+    public boolean existsByID(int testID) {
+        return false; // implement later
     }
 }
--- src/use_cases/getApiQuestions/GetApiQuestionsDataAccessInterface.java
+++ src/use_cases/getApiQuestions/GetApiQuestionsDataAccessInterface.java
@@ -1,5 +1,10 @@
 package src.use_cases.getApiQuestions;
 
+import src.entity.Test;
+
 public interface GetApiQuestionsDataAccessInterface {
     //api access is static
+    void save(Test test);
+
+    boolean existsByID(int testID);
 }
