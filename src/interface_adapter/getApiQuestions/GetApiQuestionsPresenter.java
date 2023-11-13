f3aee6ae8587250d5d0bf101fb53311818a1b170
lveluesl <jessica.shenlue@gmail.com>
2023-11-13 09:09:19 -0500

    getAPI Presenter partial

--- src/interface_adapter/getApiQuestions/GetApiQuestionsPresenter.java
+++ src/interface_adapter/getApiQuestions/GetApiQuestionsPresenter.java
@@ -1,4 +1,20 @@
 package src.interface_adapter.getApiQuestions;
 
-public class GetApiQuestionsPresenter {
+import src.use_cases.getApiQuestions.GetApiQuestionsOutputBoundary;
+import src.use_cases.getApiQuestions.GetApiQuestionsOutputData;
+
+public class GetApiQuestionsPresenter implements GetApiQuestionsOutputBoundary {
+
+    // needs view models
+    public GetApiQuestionsPresenter() {}
+
+    @Override
+    public void prepareSuccessView(GetApiQuestionsOutputData response) {
+
+    }
+
+    @Override
+    public void prepareFailView(GetApiQuestionsOutputData error) {
+
+    }
 }
