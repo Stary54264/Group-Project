49c7fb90c524a9ea94a8e34d7172354a8bbea362
heyuchengzhang <heyucheng.zhang@mail.utoronto.ca>
2023-11-20 16:13:46 -0500

    update GetResultState

--- src/interface_adapter/getResult/GetResultState.java
+++ src/interface_adapter/getResult/GetResultState.java
@@ -1,4 +1,15 @@
 package interface_adapter.getResult;
 
 public class GetResultState {
+    private String result = "";
+    public GetResultState(GetResultState copy) {
+        result = copy.result;
+    }
+    public GetResultState() {}
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
 }
