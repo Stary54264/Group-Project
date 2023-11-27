30593ca8ed15d767baf488efb95b66e28163bb31
Stary54264 <stary54264@gmail.com>
2023-11-27 17:54:11 -0500

    Update

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -2,6 +2,8 @@ package view;
 
 import interface_adapter.createOwnQuestions.CreateOwnQuestionsController;
 import interface_adapter.createOwnQuestions.CreateOwnQuestionsViewModel;
+import interface_adapter.uploadQuestions.UploadQuestionsController;
+import interface_adapter.uploadQuestions.UploadQuestionsViewModel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -14,10 +16,16 @@ public class MainView implements ActionListener, PropertyChangeListener {
     public final String viewname = "Main Menu";
     private final CreateOwnQuestionsViewModel createOwnQuestionsViewModel;
     private final CreateOwnQuestionsController createOwnQuestionsController;
+    private final UploadQuestionsController uploadQuestionsController;
     private final ArrayList<TestPanel> tests;
     private final JButton createQuestions, apiQuestions, uploadQuestions, createTest;
 
-    public MainView() {
+    public MainView(UploadQuestionsController uploadQuestionsController) {
+        this.uploadQuestionsController = uploadQuestionsController;
+        JPanel buttons = new JPanel();
+        uploadQuestions = new JButton(UploadQuestionsViewModel.UPLOAD_BUTTON_LABEL);
+        buttons.add(uploadQuestions);
+
     }
 
     public void actionPerformed(ActionEvent e) {
