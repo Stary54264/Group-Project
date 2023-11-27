5b72798e088ef24e70689bbf9a8bce871880c6f7
Stary54264 <stary54264@gmail.com>
2023-11-27 18:59:31 -0500

    Update

--- src/interface_adapter/uploadQuestions/UploadQuestionsViewModel.java
+++ src/interface_adapter/uploadQuestions/UploadQuestionsViewModel.java
@@ -6,7 +6,9 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class UploadQuestionsViewModel extends ViewModel {
-    public final String TITLE_LABEL = "Clear View";
+    public final String TITLE_LABEL = "Upload Question View";
+    public static final String TEST_NAME_LABEL = "Choose test name";
+    public static final String JSON_PATH_LABEL = "Enter the path of the file";
     public static final String UPLOAD_BUTTON_LABEL = "Upload";
     private UploadQuestionsState uploadQuestionsState = new UploadQuestionsState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
