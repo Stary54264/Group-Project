eb7d43eebf5c81c2fcdd3c1ced99a740d8b011de
heyuchengzhang <heyucheng.zhang@mail.utoronto.ca>
2023-11-27 14:58:23 -0500

    update GetResultPresenter

--- src/interface_adapter/getResult/GetResultPresenter.java
+++ src/interface_adapter/getResult/GetResultPresenter.java
@@ -1,19 +1,16 @@
 package interface_adapter.getResult;
-
-import interface_adapter.ViewManagerModel;
 import use_cases.getResult.GetResultOutputBoundary;
 import use_cases.getResult.GetResultOutputData;
+import interface_adapter.ViewManagerModel;
+
 
 public class GetResultPresenter implements GetResultOutputBoundary {
     private final GetResultViewModel getResultViewModel;
-
     private final ViewManagerModel viewManagerModel;
-
     public GetResultPresenter(ViewManagerModel viewManagerModel, GetResultViewModel getResultViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.getResultViewModel = getResultViewModel;
     }
-
     @Override
     public void prepareSuccessView(GetResultOutputData response) {
         GetResultState getResultState = getResultViewModel.getState();
