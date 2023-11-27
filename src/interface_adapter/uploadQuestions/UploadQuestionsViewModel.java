6d7f5f0b4aac61c19d2ce12e2318a24d9d4f0e40
Stary54264 <stary54264@gmail.com>
2023-11-27 17:40:22 -0500

    Update

--- src/interface_adapter/uploadQuestions/UploadQuestionsViewModel.java
+++ src/interface_adapter/uploadQuestions/UploadQuestionsViewModel.java
@@ -1,4 +1,34 @@
 package interface_adapter.uploadQuestions;
 
-public class UploadQuestionsViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class UploadQuestionsViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Clear View";
+    public static final String UPLOAD_BUTTON_LABEL = "Upload";
+    public static final String CANCEL_BUTTON_LABEL = "Cancel";
+    private UploadQuestionsState uploadQuestionsState = new UploadQuestionsState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public UploadQuestionsViewModel() {
+        super("upload questions");
+    }
+
+    public void setState(UploadQuestionsState uploadQuestionsState) {
+        this.uploadQuestionsState = uploadQuestionsState;
+    }
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.uploadQuestionsState);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public UploadQuestionsState getState() {
+        return uploadQuestionsState;
+    }
 }
