1a222943215359657eecc814a4b69a3f0fd23f16
lveluesl <jessica.shenlue@gmail.com>
2023-11-28 15:11:51 -0500

    getapi ViewModel

--- src/interface_adapter/getApiQuestions/GetApiQuestionsViewModel.java
+++ src/interface_adapter/getApiQuestions/GetApiQuestionsViewModel.java
@@ -5,6 +5,7 @@ import app.QuestionType;
 import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class GetApiQuestionsViewModel extends ViewModel {
     public static final String NAME_LABEL = "Test Name: ";
@@ -15,13 +16,28 @@ public class GetApiQuestionsViewModel extends ViewModel {
     public static final String CANCEL_BUTTON_LABEL = "Cancel";
     public static final String QUIZ_LABEL = "Take Quiz";
 
+    private GetApiQuestionsState state = new GetApiQuestionsState();
+
+    public GetApiQuestionsViewModel() {
+        super("get api questions");
+    }
+
+    public void setState(GetApiQuestionsState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+
     @Override
     public void firePropertyChanged() {
-
+        support.firePropertyChange("state", null, this.state);
     }
 
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-
+        support.addPropertyChangeListener(listener);
     }
+
+    public GetApiQuestionsState getState() {return state;}
 }
