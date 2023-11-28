b2a9db1f33df7309a11766384acede9e4ae6326f
Stary54264 <stary54264@gmail.com>
2023-11-28 00:31:49 -0500

    Update

--- src/use_cases/uploadQuestions/UploadQuestionsDataAccessInterface.java
+++ src/use_cases/uploadQuestions/UploadQuestionsDataAccessInterface.java
@@ -2,9 +2,6 @@ package use_cases.uploadQuestions;
 
 import entity.Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public interface UploadQuestionsDataAccessInterface {
     Test readTest(String testName, String jsonPath);
     void save(Test test);
