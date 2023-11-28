74a5b8a8316daabd4885be8e6e81792149e17797
Stary54264 <stary54264@gmail.com>
2023-11-28 00:53:35 -0500

    Update

--- src/data_access/UploadFileDataAccessObject.java
+++ src/data_access/UploadFileDataAccessObject.java
@@ -1,4 +1,21 @@
 package data_access;
 
+import entity.Question;
+import app.QuestionBuilder;
+import entity.Test;
+import app.TestBuilder;
+import use_cases.uploadQuestions.UploadQuestionsDataAccessInterface;
+import java.io.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class UploadFileDataAccessObject {
+    private final ArrayList<Question> questions = new ArrayList<>();
+    private final QuestionBuilder questionBuilder;
+    private final TestBuilder testBuilder;
+
+    public UploadFileDataAccessObject(QuestionBuilder questionBuilder, TestBuilder testBuilder) {
+        this.questionBuilder = questionBuilder;
+        this.testBuilder = testBuilder;
+    }
 }
