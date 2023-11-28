eb30d9440408d2375b79794cb98395a59aaf634d
lveluesl <jessica.shenlue@gmail.com>
2023-11-28 15:06:23 -0500

    getapi State

--- src/interface_adapter/getApiQuestions/GetApiQuestionsPresenter.java
+++ src/interface_adapter/getApiQuestions/GetApiQuestionsPresenter.java
@@ -1,12 +1,15 @@
 package interface_adapter.getApiQuestions;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.takeQuiz.takeQuizViewModel;
 import use_cases.getApiQuestions.GetApiQuestionsOutputBoundary;
 import use_cases.getApiQuestions.GetApiQuestionsOutputData;
 
 public class GetApiQuestionsPresenter implements GetApiQuestionsOutputBoundary {
-
-    // needs view models
-    public GetApiQuestionsPresenter() {}
+    private final GetApiQuestionsViewModel getApiQuestionsViewModel;
+    public GetApiQuestionsPresenter(GetApiQuestionsViewModel getApiQuestionsViewModel) {
+        this.getApiQuestionsViewModel = getApiQuestionsViewModel;
+    }
 
     @Override
     public void prepareSuccessView(GetApiQuestionsOutputData response) {
--- src/interface_adapter/getApiQuestions/GetApiQuestionsViewModel.java
+++ src/interface_adapter/getApiQuestions/GetApiQuestionsViewModel.java
@@ -1,4 +1,27 @@
 package interface_adapter.getApiQuestions;
 
-public class GetApiQuestionsViewModel {
+import app.QuestionDifficulty;
+import app.QuestionType;
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class GetApiQuestionsViewModel extends ViewModel {
+    public static final String NAME_LABEL = "Test Name: ";
+    public static final String NUMBER_LABEL = "Number of Questions: ";
+    public static final String CATEGORY_LABEL = "Choose Category: ";
+    public static final String TYPE_LABEL = "Choose Type: ";
+    public static final String DIFF_LABEL = "Choose Difficulty: ";
+    public static final String CANCEL_BUTTON_LABEL = "Cancel";
+    public static final String QUIZ_LABEL = "Take Quiz";
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
