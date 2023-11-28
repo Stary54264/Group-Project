0a9ffe81518ed45d11bb4cf99f34425bed3eb8c6
Stary54264 <stary54264@gmail.com>
2023-11-27 21:51:21 -0500

    Update

---
+++ src/data_access/FileTestDataAccessObject.java
@@ -0,0 +1,36 @@
+package data_access;
+
+import entity.Test;
+
+import use_cases.createOwnQuestions.CreateOwnQuestionsDataAccessInterface;
+import use_cases.getApiQuestions.GetApiQuestionsDataAccessInterface;
+import use_cases.takeQuiz.takeQuizDataAccessInterface;
+import use_cases.uploadQuestions.UploadQuestionsUserDataAccessInterface;
+
+public class FileTestDataAccessObject implements takeQuizDataAccessInterface,
+        GetApiQuestionsDataAccessInterface,
+        CreateOwnQuestionsDataAccessInterface,
+        UploadQuestionsUserDataAccessInterface {
+
+    @Override
+    public Test getTest(String name) {
+        // TODO fmwof
+        return null;
+    }
+
+    @Override
+    public Test readTest(String testName, String jsonPath) {
+        // TODO stary
+        return null;
+    }
+
+    @Override
+    public void save(Test test) {
+        // TODO fmwof
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        // TODO fmwof
+    }
+}
