561af8e659b94b01abf376bd287f61d99dd62b42
Stary54264 <stary54264@gmail.com>
2023-11-28 00:53:52 -0500

    Update

--- src/data_access/UploadFileDataAccessObject.java
+++ src/data_access/UploadFileDataAccessObject.java
@@ -18,4 +18,23 @@ public class UploadFileDataAccessObject {
         this.questionBuilder = questionBuilder;
         this.testBuilder = testBuilder;
     }
+
+    public Test readTest(String testName, String csvPath) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(csvPath)));
+            String row;
+            while ((row = reader.readLine()) != null) {
+                String[] col = row.split(",");
+                ArrayList<String> incorrectAnswers = new ArrayList<>(Arrays.asList(
+                        String.valueOf(col[2]), String.valueOf(col[3]), String.valueOf(col[4])));
+                testBuilder.addQuestion(questionBuilder.
+                        setQuestionText(String.valueOf(col[0])).
+                        setCorrectAnswer(String.valueOf(col[1])).
+                        setIncorrectAnswers(incorrectAnswers).build());
+            }
+        }
+        catch (Exception E) {
+        }
+        return(testBuilder.setName(testName).setCategory("General").setComment("").build());
+    }
 }
