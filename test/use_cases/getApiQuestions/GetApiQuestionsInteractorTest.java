65d29a8930845d02e2a45f7593561408bf5e77af
lveluesl <jessica.shenlue@gmail.com>
2023-12-03 18:50:30 -0500

    GetApiQuestionsInteractorTest start

--- test/use_cases/getApiQuestions/GetApiQuestionsInteractorTest.java
+++ test/use_cases/getApiQuestions/GetApiQuestionsInteractorTest.java
@@ -1,12 +1,20 @@
 package use_cases.getApiQuestions;
 
+import app.Category;
+import app.QuestionDifficulty;
+import app.QuestionType;
+import data_access.APIDataAccessObject;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GetApiQuestionsInteractorTest {
-
     @Test
-    void execute() {
+    void successTest() {
+        GetApiQuestionsInputData inputData = new GetApiQuestionsInputData(2, Category.AnyCategory,
+                QuestionType.BOOL, QuestionDifficulty.EASY, "QUIZ #1");
+
+
     }
 }
