36d1665061614db4b7e0b6426505b42e0047f684
Stary54264 <stary54264@gmail.com>
2023-12-03 21:02:22 -0500

    Update

--- test/use_cases/getApiQuestions/GetApiQuestionsInteractorTest.java
+++ test/use_cases/getApiQuestions/GetApiQuestionsInteractorTest.java
@@ -24,46 +24,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class GetApiQuestionsInteractorTest {
 
     @Test
-    void executeSuccessTest() {
+    void executeSuccessTest() throws InterruptedException {
+        Thread.sleep(5000);
         FileTestDataAccessObject dataAccessInterface = new FileTestDataAccessObject();
+        GetApiQuestionsViewModel getApiQuestionsViewModel = new GetApiQuestionsViewModel();
+        takeQuizViewModel takeQuizViewModel = new takeQuizViewModel();
         GetApiQuestionsPresenter getApiQuestionsPresenter = new GetApiQuestionsPresenter(
-                new GetApiQuestionsViewModel(), new takeQuizViewModel()
-        );
-        GetApiQuestionsInputData getApiQuestionsInputData = new GetApiQuestionsInputData(
-                10,
+                getApiQuestionsViewModel, takeQuizViewModel);
+        GetApiQuestionsInputBoundary getApiQuestionsInteractor = new GetApiQuestionsInteractor(dataAccessInterface,
+                getApiQuestionsPresenter);
+        GetApiQuestionsController getApiQuestionsController = new GetApiQuestionsController(getApiQuestionsInteractor);
+        getApiQuestionsController.execute(10,
                 Category.AnyCategory,
                 QuestionType.ALL,
                 QuestionDifficulty.ALL,
                 "Api testing test");
-        GetApiQuestionsInputBoundary getApiQuestionsInteractor = new GetApiQuestionsInteractor(dataAccessInterface,
-                getApiQuestionsPresenter);
-        getApiQuestionsInteractor.execute(getApiQuestionsInputData);
+        GetApiQuestionsState getApiQuestionsState = new GetApiQuestionsState();
+        getApiQuestionsViewModel.setState(getApiQuestionsState);
+        getApiQuestionsState.getTestNameError();
+        getApiQuestionsState.getNumber();
+        getApiQuestionsState.getCategory();
+        getApiQuestionsState.getType();
+        getApiQuestionsState.getDiff();
+        getApiQuestionsState.setTestName("Api testing test");
+        getApiQuestionsState.setNumber(10);
+        getApiQuestionsState.setCategory(Category.AnyCategory);
+        getApiQuestionsState.setType(QuestionType.ALL);
+        getApiQuestionsState.setDiff(QuestionDifficulty.ALL);
+        assertEquals("Api testing test", getApiQuestionsState.getTestName());
         dataAccessInterface.deleteTest("Api testing test");
     }
 
     @Test
     void executeFailTest() {
         FileTestDataAccessObject dataAccessInterface = new FileTestDataAccessObject();
-        GetApiQuestionsOutputBoundary getApiQuestionsPresenter =new GetApiQuestionsOutputBoundary() {
-            @Override
-            public void prepareSuccessView(GetApiQuestionsOutputData getApiQuestionsOutputData) {
-                fail("Use case successful is unexpected.");
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                assertEquals("Failed!", error);
-            }
-        };
-        GetApiQuestionsInputData getApiQuestionsInputData = new GetApiQuestionsInputData(
-                10,
+        GetApiQuestionsViewModel getApiQuestionsViewModel = new GetApiQuestionsViewModel();
+        takeQuizViewModel takeQuizViewModel = new takeQuizViewModel();
+        GetApiQuestionsPresenter getApiQuestionsPresenter = new GetApiQuestionsPresenter(
+                getApiQuestionsViewModel, takeQuizViewModel);
+        GetApiQuestionsInputBoundary getApiQuestionsInteractor = new GetApiQuestionsInteractor(dataAccessInterface,
+                getApiQuestionsPresenter);
+        GetApiQuestionsController getApiQuestionsController = new GetApiQuestionsController(getApiQuestionsInteractor);
+        getApiQuestionsController.execute(0,
                 Category.AnyCategory,
                 QuestionType.ALL,
                 QuestionDifficulty.ALL,
                 "Api testing test");
-        GetApiQuestionsInputBoundary getApiQuestionsInteractor = new GetApiQuestionsInteractor(dataAccessInterface,
-                getApiQuestionsPresenter);
-        getApiQuestionsInteractor.execute(getApiQuestionsInputData);
         dataAccessInterface.deleteTest("Api testing test");
     }
 }
